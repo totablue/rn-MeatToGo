ceb712a41795a0bf61e3dfac5b50c02d76d8d9b8
Tomo <tomoki_ota@outlook.com>
2022-12-04 22:20:51 +0900

    navigation, icons, flatlist

--- src/features/restaurants/components/RestauranInfoCard.styled.js
+++ src/features/restaurants/components/RestauranInfoCard.styled.js
@@ -6,11 +6,12 @@ export const InfoContainer = styled.View`
 `;
 
 export const RestaurantCard = styled(Card)`
-    background-color: white;
+    background-color: ${(props) => props.theme.colors.bg.primary};
+    margin-bottom: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantCardCover = styled(Card.Cover)`
-    background-color: white;
+    background-color: ${(props) => props.theme.colors.bg.primary};
     padding: ${(props) => props.theme.space[3]};
 `;
 
